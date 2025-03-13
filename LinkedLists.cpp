1cafde93ffba2e0b3bd6916b02ba8a26e086b5a0
Brandon <41562804+bthecobb@users.noreply.github.com>
2025-03-13 13:04:48 -0700

    Add files via upload

--- LinkedLists.cpp
+++ LinkedLists.cpp
@@ -1,16 +1,17 @@
-#include <iostream>  l
-using namespace std;
+#include <iostream>  
+using namespace std;  // Ensures cout, endl are accessible
 
 struct Node {
     int value;
     Node* next;
-    Node(int val) : value(val), next(nullptr) {}  // Constructor
+    Node(int val) : value(val), next(nullptr) {}
 };
 
 void printList(Node* head) {
     while (head) {
-        cout << head->value << " -> ";
+        cout << head->value << " -> ";  // Now works
         head = head->next;
     }
-    cout << "nullptr" << endl;
+    cout << "nullptr" << endl;  // Now works
 }
+
